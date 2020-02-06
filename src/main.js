781485df46382801debb85740f3d12f1b7d48c86
Oleh Shevchuk <olehshevtchuk@gmail.com>
2020-02-06 15:18:17 +0200

    added connection to db through sequelize

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ const path = require('path');
 import Koa from 'koa';
 import cors from '@koa/cors';
 import bodyParser from 'koa-body';
-// import db from './db';
+import db from './db';
 // import appRouter from './routes';
 
 // import errorHandler from './middlewares/errorHandler';
 
-// const { sequelize } = db;
+const { sequelize } = db;
 
 const PORT = 3030;
 
@@ -19,19 +19,19 @@ app.use(bodyParser());
 app.use(cors());
 // app.use(appRouter.routes());
 
-
-// sequelize.sync().then(async () => {
-//     app.listen(config.port, () => {
-//         startCrons();
-//         console.log(`server listen on port ${config.port}`);
-//     });
-// });
-
-
 app.use(function* () {
     this.body = "Hellod World !!!";
 });
 
-app.listen(PORT, () => {
-    console.log(`server listen on port ${PORT}`);
+sequelize.sync().then(async () => {
+    app.listen(PORT, () => {
+        console.log(`server listen on port ${PORT}`);
     });
+});
+
+
+
+
+// app.listen(PORT, () => {
+//     console.log(`server listen on port ${PORT}`);
+//     });

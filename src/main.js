e2da17e1d4be5ca83374bf69abaf037698e7f37f
Oleh Shevchuk <olehshevtchuk@gmail.com>
2020-02-07 19:25:34 +0200

    added errorHandler middleware

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import bodyParser from 'koa-body';
 import db from './db';
 import appRouter from './routes';
 
-// import errorHandler from './middlewares/errorHandler';
+import errorHandler from './middlewares/errorHandler';
 
 const { sequelize } = db;
 
@@ -14,24 +14,13 @@ const PORT = 3030;
 
 const app = new Koa();
 
-// app.use(errorHandler);
+app.use(errorHandler);
 app.use(bodyParser());
 app.use(cors());
 app.use(appRouter.routes());
-
-app.use(function* () {
-    this.body = "Hellod World !!!";
-});
 
 sequelize.sync().then(async () => {
     app.listen(PORT, () => {
         console.log(`server listen on port ${PORT}`);
     });
 });
-
-
-
-
-// app.listen(PORT, () => {
-//     console.log(`server listen on port ${PORT}`);
-//     });
